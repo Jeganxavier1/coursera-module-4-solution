3a271916cfab4406b199388834e8e2bee5873aa6
Jeganxavier1 <97794370+Jeganxavier1@users.noreply.github.com>
2022-08-04 18:02:45 +0530

    Add files via upload

---
+++ module-4.js
@@ -0,0 +1,9 @@
+var names=["alice","Jack","mock","elon","jason","robert","buffet","jack ma"]
+var result;
+for(var i=0;i<names.length;i++){
+    if(names[i][0] ==="J" || names[i][0] ==="j"){
+        console.log("goodbye "+ names[i]);
+    }else{
+        console.log("hello "+ names[i])
+    }
+}
